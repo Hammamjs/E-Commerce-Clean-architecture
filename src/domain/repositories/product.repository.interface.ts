4343e208804acd21168de2d3ee9640c308cb75e9
Hammam Hussein <hamamhussein10@gmail.com>
2026-01-28 22:23:12 +0300

    Domain/product interface

--- src/domain/repositories/product.repository.interface.ts
+++ src/domain/repositories/product.repository.interface.ts
@@ -1,14 +1,14 @@
 import { Products } from '../entities/products.entity';
 
 export interface IProductRepository {
-  findProduct(productId: string): Promise<Products | null>;
-  findAll(): Promise<Products[]>;
-  create(product: Products): Promise<Products | null>;
-  update(product: Products): Promise<Products | null>;
-  delete(productId: string): Promise<Products | null>;
-  increaseStockWitTx(
-    productId: string,
-    quantity: number,
-  ): Promise<{ inStock: number }>;
-  decreaseStockWitTx(productId: string, quantity: number): Promise<Products>;
+ findProduct(productId: string): Promise<Products | null>;
+ findAll(): Promise<Products[]>;
+ create(product: Products): Promise<Products | null>;
+ update(product: Products): Promise<Products | null>;
+ delete(productId: string): Promise<Products | null>;
+ increaseStockWithTx(
+  productId: string,
+  quantity: number,
+ ): Promise<{ inStock: number }>;
+ decreaseStockWithTx(productId: string, quantity: number): Promise<Products>;
 }
